33c2ca9b476374af10a32738a202af7a1983a129
Aurelien-jcb <jacob-aurelien@hotmail.fr>
2021-06-02 22:06:59 +0200

    update before delete

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-let input__alert = document.querySelector('.input__alert');
+'use strict';
+
+const input__alert = document.querySelector('.input__alert');
 
 function generatePassword() {
-    let chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+?><:{}[] ";
-    let passwordLenght = 16;
+    const chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()/|_+?:[] ";
+    const passwordLenght = document.getElementById("passwordLenght").value;
     let password = '';
 
     for (let i = 0; i < passwordLenght; i++) {
-        let randomNumber = Math.floor(Math.random() * chars.length);
+        const randomNumber = Math.floor(Math.random() * chars.length);
         password += chars.substring(randomNumber, randomNumber + 1);
     }
     document.getElementById("password").value = password;
@@ -14,13 +16,12 @@ function generatePassword() {
 }
 
 function copyPassword() {
-    let copyPassText = document.getElementById("password");
+    const copyPassText = document.getElementById("password");
     copyPassText.select();
     copyPassText.setSelectionRange(0, 9999);
     document.execCommand('copy');
     input__alert.classList.toggle('active');
-    setTimeout(function() 
-    {
+    setTimeout(function () {
         input__alert.classList.toggle('active');
-    },2000)
+    }, 2000)
 }
